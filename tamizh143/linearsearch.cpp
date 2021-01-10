642dce6303a912fdcbb07cff40b6aa9b064d3b2a
tamizh143 <50977879+tamizh143@users.noreply.github.com>
2021-01-10 19:14:15 +0530

    Update linearsearch.cpp

--- tamizh143/linearsearch.cpp
+++ tamizh143/linearsearch.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int linearSearch(int arr[],int key,int length)
+{
+    for(int iter=0;iter<length;iter++)
+    {
+        if(arr[iter]==key)
+        {
+           return 1;
+        }
+       
+    }
+    return 0;
+}
+
+int main()
+{
+    int arr[] = {1,2,3,7,43,2,4,64};
+    int key=3;
+    int length = sizeof(arr)/sizeof(arr[0]);
+    if(linearSearch(arr,key,length))
+    {
+        std::cout << "Key Found" << std::endl;
+    }
+    else
+    {
+        std::cout << "Key Not Found" << std::endl;
+    }  
+}
+
+
 
